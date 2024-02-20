b05088ac5c0206a6ab5962fb4866414477fe5229
Jakub Grzybowski <jakub.grzybowski@microsoft.wsei.edu.pl>
2024-02-20 18:32:32 +0100

    zatwierdam

---
+++ LibApp/Controllers/Api/BookApiController.cs
@@ -0,0 +1,42 @@
+﻿using LibApp.Data;
+using LibApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+       
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+    }
+}
---
+++ LibApp/Controllers/Api/ControllersApi.cs
@@ -0,0 +1,46 @@
+﻿using LibApp.Data;
+using LibApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/customers/details/5
+        [HttpGet("details/{id}")]
+        public IActionResult CustomerDetails(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // GET: api/customers/edit/5
+        [HttpGet("edit/{id}")]
+        public IActionResult EditCustomer(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+    }
+}
